4a5f37fb8595bf4c4029af307636b8382d7ffac2
maple <datongde@gmail.com>
2017-01-22 06:22:09 +0800

    fix search function, and sort function,and will_paginate.Now it can not sort search resoult.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,25 +4,29 @@ class JobsController < ApplicationController
     @job = Job.find(params[:id])
 
     if @job.is_hidden
-      flash[:warning] = "This Job already archieved"
+      flash[:warning] = 'This Job already archieved'
       redirect_to root_path
     end
   end
+
   def index
     # @jobs = Job.where(:is_hidden => false).order("created_at DESC")
     @jobs = case params[:order]
             when 'by_lower_bound'
-              Job.published.order("wage_lower_bound DESC")
+              Job.published.order('wage_lower_bound DESC').search(params[:search]).paginate(page: params[:page], per_page: 5)
             when 'by_upper_bound'
-              Job.published.order("wage_upper_bound DESC")
+              Job.published.order('wage_upper_bound DESC').search(params[:search]).paginate(page: params[:page], per_page: 5)
             else
-              Job.published.recent.search(params[:search])
-            end
-    # @jobs = Job.published.search(params[:search])
+              Job.published.recent.search(params[:search]).paginate(page: params[:page], per_page: 5)
+         end
+
+    # @jobs = Job.published.search(params[:search]) //如果使用两次则只有下边这一句有效。
   end
+
   def new
     @job = Job.new
   end
+
   def create
     @job = Job.new(job_params)
     if @job.save
@@ -31,9 +35,11 @@ class JobsController < ApplicationController
       render :new
     end
   end
+
   def edit
     @job = Job.find(params[:id])
   end
+
   def update
     @job = Job.find(params[:id])
     if @job.update(job_params)
@@ -42,12 +48,15 @@ class JobsController < ApplicationController
       render :edit
     end
   end
+
   def destroy
     @job = Job.find(params[:id])
     @job.destroy
     redirect_to jobs_path
   end
+
   private
+
   def job_params
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
   end
